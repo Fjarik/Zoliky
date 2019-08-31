a8cd19cc7e11059104e4dd93335dff27d3335962
Jiri Falta <jirkafalta@gmail.com>
2019-08-31 13:19:33 +0200

    GraphQL - WakeUp

---
+++ ApiGraphQL/Controllers/PublicController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGraphQL.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class PublicController : ControllerBase
+	{
+		// GET /public/connection
+		[HttpGet]
+		[Route("connection")]
+		public ActionResult<bool> Connection()
+		{
+			return Ok(true);
+		}
+	}
+}
